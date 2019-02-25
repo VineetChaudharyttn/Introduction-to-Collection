171199cf7a8557d8a2018b6748dcf8bf3f5be8dc
VineetChaudharyttn <vineet.chaudhary@tothenew.com>
2019-02-25 19:27:33 +0530

    Update

--- src/Question6.java
+++ src/Question6.java
@@ -5,51 +5,37 @@ import java.util.*;
 
 public class Question6 {
     public static void main(String[] args) {
-        System.out.println("Enter the numbers of list:");
+        System.out.println("Enter the number of elements:");
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arrayList=new ArrayList<>();
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-        arrayList.add(sc.nextInt());
-
-        Map<Integer,Integer> arrayIndex=new HashMap<>();
-        Integer count=0;
-        Integer value,flag,j;
-
-        arrayIndex.put(0,1);
-        for(int i =1; i<arrayList.size();i++){
-            value=0;
-            flag=0;
-            for ( Integer key : arrayIndex.keySet() ) {
-                    if(arrayList.get(key)==arrayList.get(i)){
-                    value=arrayIndex.get(key);
-                    arrayIndex.put(key,++value);
-                    flag=1;
-                }
-            }
-            if(flag==0){
-                arrayIndex.put(i,1);
-            }
+        int size=sc.nextInt();
+        List<Integer> number=new ArrayList<>();
+        for (int i=0;i<size;i++){
+            number.add(sc.nextInt());
         }
 
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i =0; i<size;i++){
+                if (map.containsKey(number.get(i))) {
+                    map.put(number.get(i),map.get(number.get(i))+1);
+                }
+                else{
+                    map.put(number.get(i),1);
+                }
 
-        List<Map.Entry<Integer,Integer>> list=new LinkedList<Map.Entry<Integer, Integer>>(arrayIndex.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+        }
+        List<Map.Entry<Integer,Integer>> list=new LinkedList<Map.Entry<Integer, Integer>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer,Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-
-                return o2.getValue().compareTo(o1.getValue());
+                return  o2.getValue().compareTo(o1.getValue());
             }
+
         });
         for (Map.Entry<Integer,Integer> entry:list
              ) {
-                System.out.println("Element"+arrayList.get(entry.getKey())+"frequency"+entry.getValue());
+            System.out.println("Element"+entry.getKey()+"frequency"+entry.getValue());
         }
+
+
     }
 }
